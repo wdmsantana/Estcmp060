8cab6cfe93267c789577177f4fa2b2b30321a14e
Wanderberg <83989063+wdmsantana@users.noreply.github.com>
2021-05-12 15:04:16 -0400

    Update pong.py

--- pong.py
+++ pong.py
@@ -5,7 +5,7 @@ import os
 screen = turtle.Screen()
 screen.title("My Pong")
 screen.bgcolor("black")
-screen.setup(width=1270, height=720)
+screen.setup(width=1150, height=720)
 screen.tracer(0)
 
 # draw paddle 1
@@ -28,13 +28,13 @@ paddle_2.goto(500, 0)
 
 # draw ball
 ball = turtle.Turtle()
-ball.speed(-1)
+ball.speed(0)
 ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 1
-ball.dy = 1
+ball.dx = 0.50
+ball.dy = 0.50
 
 # score
 score_1 = 0
@@ -50,94 +50,3 @@ hud.hideturtle()
 hud.goto(0, 260)
 hud.write("0 : 0", align="center", font=("Press Start 2P", 24, "normal"))
 
-
-def paddle_1_up():
-    y = paddle_1.ycor()
-    if y < 250:
-        y += 30
-    else:
-        y = 250
-    paddle_1.sety(y)
-
-
-def paddle_1_down():
-    y = paddle_1.ycor()
-    if y > -250:
-        y += -30
-    else:
-        y = -250
-    paddle_1.sety(y)
-
-
-def paddle_2_up():
-    y = paddle_2.ycor()
-    if y < 250:
-        y += 30
-    else:
-        y = 250
-    paddle_2.sety(y)
-
-
-def paddle_2_down():
-    y = paddle_2.ycor()
-    if y > -250:
-        y += -30
-    else:
-        y = -250
-    paddle_2.sety(y)
-
-
-# keyboard
-screen.listen()
-screen.onkeypress(paddle_1_up, "w")
-screen.onkeypress(paddle_1_down, "s")
-screen.onkeypress(paddle_2_up, "Up")
-screen.onkeypress(paddle_2_down, "Down")
-
-while True:
-    screen.update()
-
-    # ball movement
-
-    ball.setx(ball.xcor() + ball.dx)
-    ball.sety(ball.ycor() + ball.dy)
-
-    # collision with the upper wall
-    if ball.ycor() > 290:
-        os.system("bounce.wav&")
-        ball.sety(290)
-        ball.dy *= -1
-
-    # collision with lower wall
-    if ball.ycor() < -290:
-        os.system("bounce.wav&")
-        ball.sety(-290)
-        ball.dy *= -1
-
-    # collision with left wall
-    if ball.xcor() < -501:
-        score_2 += 1
-        hud.clear()
-        hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
-        os.system("258020__kodack__arcade-bleep-sound.wav&")
-        ball.goto(0, 0)
-        ball.dx *= -1
-
-    # collision with right wall
-    if ball.xcor() > 501:
-        score_1 += 1
-        hud.clear()
-        hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
-        os.system("258020__kodack__arcade-bleep-sound.wav&")
-        ball.goto(0, 0)
-        ball.dx *= -1
-
-    # collision with the paddle 1
-    if ball.xcor() < - 450 and paddle_1.ycor() + 50 > ball.ycor() > paddle_1.ycor() - 50:
-        ball.dx *= -1
-        os.system("bounce.wav&")
-
-    # collision with the paddle 2
-    if ball.xcor() > 450 and paddle_2.ycor() + 50 > ball.ycor() > paddle_2.ycor() - 50:
-        ball.dx *= -1
-        os.system("bounce.wav&")
